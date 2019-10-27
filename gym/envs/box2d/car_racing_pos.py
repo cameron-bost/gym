c7afd2ea2327c317b5c679f71827b87a1a368dd1
Cameron Bost <cameron.a.bost@gmail.com>
2019-10-26 20:53:46 -0500

    Bug Fix - random IndexError in step()

--- gym/envs/box2d/car_racing_pos.py
+++ gym/envs/box2d/car_racing_pos.py
@@ -1147,6 +1147,8 @@ class CarRacingPoSContinuousState(gym.Env, EzPickle):
 
             except StopIteration:
                 pass
+            except IndexError:
+                pass
 
             # Small negative reward based on raycast distance to wall
             raycast_reward = -.5 + 0.5 / (75 - 10) * (sum(raycast_dist_state) - 10) / (speed_state / 10 + 1)
