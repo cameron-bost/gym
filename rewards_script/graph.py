1d2bf4f7f540f523f4890606c18b16b5312f679e
Bryce Hennen <ecyrbnenneh@gmail.com>
2019-10-27 23:50:28 -0500

    update graph py

--- rewards_script/graph.py
+++ rewards_script/graph.py
@@ -3,9 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
 
-EXPERIMENT1 = "double_sarsa"
-EXPERIMENT2 = "single_sarsa"
-EXPERIMENT_TITLE = "Single Expected Sarsa vs Double Expected Sarsa"
+EXPERIMENT1 = "final_single_e_sarsa"
+EXPERIMENT2 = "human"
+EXPERIMENT3 = "random"
+EXPERIMENT_TITLE = "Single E-SARSA vs Baselines"
+EXPERIMENT_SUBTITLE = "10000 Learning Episodes, 40 Random Trials"
+CUTOFF_MAX_EPISODES = 30000 # Only consider data up to the cutoff
+use_last_n_episodes = 40  # Only use the last 40 episodes from the dataset
 
 def get_list_of_rewards_files(rewards_directory, experiment_name, metric_name):
     cur_path = os.path.dirname(rewards_directory)
@@ -50,7 +54,8 @@ def get_data(rewards_directory, experiment_name, metric_name):
             group.append((range(len(y)), y))
         return group
 
-def graph_data(rewards_directory, experiment_name, metric_name, start_color, end_color, filter_fn=lambda x: True):
+
+def graph_data(rewards_directory, experiment_name, metric_name, start_color, end_color, filter_fn=lambda x: True, regression_color=None):
     data = get_data(rewards_directory, experiment_name, metric_name)
     
     fig = plt.figure(0)
@@ -67,8 +72,10 @@ def graph_data(rewards_directory, experiment_name, metric_name, start_color, end
                 filtered_data[0].append(dataset[0][index])
                 filtered_data[1].append(y_val)
         x, y = filtered_data
-        x = np.asarray(x)
-        y = np.asarray(y)
+        start_index = CUTOFF_MAX_EPISODES - use_last_n_episodes if len(x) > CUTOFF_MAX_EPISODES else len(x) - use_last_n_episodes
+        x = np.asarray(x[start_index:CUTOFF_MAX_EPISODES])
+        x = x - np.min(x) # go from 0 to len(x)
+        y = np.asarray(y[start_index:CUTOFF_MAX_EPISODES])
         if metric_name == "tiles_visited":
             if episode == 0:
                 plt.plot(x, y, color=color, label = "Early Episodes")
@@ -82,7 +89,7 @@ def graph_data(rewards_directory, experiment_name, metric_name, start_color, end
             # Lowess Smoothing Regression
             frac = 1.0/10
             fitted_vals = sm.nonparametric.lowess(y, x, frac = frac, return_sorted=False)
-            plt.plot(x, fitted_vals, color=end_color, label=f"{experiment_name} - Lowess Regression")
+            plt.plot(x, fitted_vals, color=regression_color, label=f"{experiment_name} - Lowess Regression")
             plt.legend()
             
             # # Rolling Average 
@@ -115,24 +122,37 @@ def graph_data(rewards_directory, experiment_name, metric_name, start_color, end
         plt.ylabel('Tiles')
     
 def graph_finish():
-    plt.title(EXPERIMENT_TITLE)
+    plt.suptitle(EXPERIMENT_TITLE)
+    plt.title(EXPERIMENT_SUBTITLE)
     plt.show()
 
 
 
 if __name__ == "__main__":
-    cur_dir = os.path.dirname(__file__)
-    rewards_directory = os.path.join(cur_dir, f"qlearn_results\\")
-    # graph_data(rewards_directory, EXPERIMENT1, "tiles_per_iter", filter_fn=lambda x: x < 0.5)
-    # graph_data(rewards_directory, EXPERIMENT2, "tiles_visited")
-
-    start_color1 = np.array((255, 100, 0, 50)) / 255.0
-    end_color1 = np.array((255, 0, 0, 255)) / 255.0 
     #End color is the loess regression line color for tiles_per_iter, otherwise it is the last episodes tiles_visited line color
+    cur_dir = os.path.dirname(__file__)
+    rewards_directory = os.path.join(cur_dir, f"rewards\\")
+    
+    start_color1 = np.array((33, 32, 117, 50)) / 255.0
+    end_color1 = np.array((128, 0, 0, 255)) / 255.0 
+    regression_color1 = np.array((33, 32, 117, 255)) / 255.0
+    graph_data(rewards_directory, EXPERIMENT1, "tiles_per_iter", start_color1, end_color1, filter_fn=lambda x: x < 0.3, regression_color=regression_color1)
+    
+    start_color2 = np.array((11, 163, 120, 50)) / 255.0
+    end_color2 = np.array((128, 0, 0, 255)) / 255.0
+    regression_color2 = np.array((11, 163, 120, 255)) / 255.0
+    graph_data(rewards_directory, EXPERIMENT2, "tiles_per_iter", start_color2, end_color2, filter_fn=lambda x: x < 0.3, regression_color=regression_color2)
+    
+    start_color3 = np.array((199, 149, 10, 50)) / 255.0
+    end_color3 = np.array((128, 0, 0, 255)) / 255.0
+    regression_color3 = np.array((199, 149, 10, 255)) / 255.0
+    graph_data(rewards_directory, EXPERIMENT3, "tiles_per_iter", start_color3, end_color3, filter_fn=lambda x: x < 0.3, regression_color=regression_color3)
 
-    start_color2 = np.array((0, 150, 200, 50)) / 255.0
-    end_color2 = np.array((0, 0, 255, 255)) / 255.0
-
-    graph_data(rewards_directory, EXPERIMENT1, "tiles_per_iter", start_color1, end_color1, filter_fn=lambda x: x < 0.5)
-    graph_data(rewards_directory, EXPERIMENT2, "tiles_per_iter", start_color2, end_color2, filter_fn=lambda x: x < 0.5)
     graph_finish()
+
+    # start_color2 = np.array((0, 150, 200, 50)) / 255.0
+    # end_color2 = np.array((0, 0, 255, 255)) / 255.0
+
+    # graph_data(rewards_directory, EXPERIMENT1, "tiles_per_iter", start_color1, end_color1, filter_fn=lambda x: x < 0.5)
+    # graph_data(rewards_directory, EXPERIMENT2, "tiles_per_iter", start_color2, end_color2, filter_fn=lambda x: x < 0.5)
+    # graph_finish()
